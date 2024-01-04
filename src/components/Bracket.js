fe228cbc49a28dc5cbb83506f7852f06666610da
carlortiz <mr.carlortiz@gmail.com>
2024-01-03 20:07:03 -0800

    created bracket component

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Bracket.css';
 
 function Bracket() {
     return (
-        <div>
-        <h1>Bracket</h1>
+      <div>
+            <div className="bracket">
+                <div className="matchup">
+                    <div className="team">
+                        <div className="team">Team 1</div>
+                        <div className="team">Team 2</div>
+                    </div>
+                    <div className="team">
+                        <div className="team">Team 3</div>
+                        <div className="team">Team 4</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Matchup.js
+++ src/components/Matchup.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Matchup() {
-    return (
-        <div>
-        <h1>Matchup</h1>
-        </div>
-    );
+  return (
+    <div>
+    <h1>Matchup</h1>
+    </div>
+  );
 }
 
 export default Matchup;
