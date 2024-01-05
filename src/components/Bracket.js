78516126977ec950be57fa3477443bf68cc78059
carlortiz <mr.carlortiz@gmail.com>
2024-01-04 20:52:22 -0800

    added team assets

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -5,16 +5,7 @@ function Bracket() {
     return (
       <div>
             <div className="bracket">
-                <div className="matchup">
-                    <div className="team">
-                        <div className="team">Team 1</div>
-                        <div className="team">Team 2</div>
-                    </div>
-                    <div className="team">
-                        <div className="team">Team 3</div>
-                        <div className="team">Team 4</div>
-                    </div>
-                </div>
+                <div className="team">Placeholder</div>
             </div>
         </div>
     );
