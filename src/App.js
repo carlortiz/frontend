3a0318e944f6bccbb9a1e4e0e06214d3c0b23af1
carlortiz <you@example.com>
2024-01-11 20:11:28 -0800

    displayed local data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './App.css'
 import bills_logo from './assets/bills_logo.webp'
 import steelers_logo from './assets/steelers_logo.webp'
+import browns_logo from './assets/browns_logo.jpeg'
+import buccaneers_logo from './assets/buccaneers_logo.png'
+import chiefs_logo from './assets/chiefs_logo.webp'
+import cowboys_logo from './assets/cowboys_logo.webp'
+import lions_logo from './assets/lions_logo.jpeg'
+import dolphins_logo from './assets/dolphins_logo.webp'
+import eagles_logo from './assets/eagles_logo.webp'
+import niners_logo from './assets/niners_logo.avif'
+import packers_logo from './assets/packers_logo.jpeg'
+import rams_logo from './assets/rams_logo.png'
+import texans_logo from './assets/texans_logo.avif'
+import ravens_logo from './assets/ravens_logo.jpeg'
+import superbowl_logo from './assets/superbowl_lvii.png'
 
 function App() {
   return (
@@ -15,76 +28,90 @@ function App() {
       </div>
 
       <div className="bracket chiefs">
-        <img src={bills_logo} className='team'></img>
+        <img src={chiefs_logo} className='team'></img>
       </div>
 
       <div className="bracket dolphins">
-        <img src={steelers_logo} className='team'></img>
+        <img src={dolphins_logo} className='team'></img>
       </div>
 
       <div className="bracket texans">
-        <img src={bills_logo} className='team'></img>
+        <img src={texans_logo} className='team'></img>
       </div>
 
       <div className="bracket browns">
-        <img src={steelers_logo} className='team'></img>
+        <img src={browns_logo} className='team'></img>
       </div>
 
       <div className="bracket cowboys">
-        <img src={bills_logo} className='team'></img>
+        <img src={cowboys_logo} className='team'></img>
       </div>
 
       <div className="bracket packers">
-        <img src={steelers_logo} className='team'></img>
+        <img src={packers_logo} className='team'></img>
       </div>
 
       <div className="bracket lions">
-        <img src={bills_logo} className='team'></img>
+        <img src={lions_logo} className='team'></img>
       </div>
 
       <div className="bracket rams">
-        <img src={steelers_logo} className='team'></img>
+        <img src={rams_logo} className='team'></img>
       </div>
 
       <div className="bracket buccaneers">
-        <img src={bills_logo} className='team'></img>
+        <img src={buccaneers_logo} className='team'></img>
       </div>
 
       <div className="bracket eagles">
-        <img src={steelers_logo} className='team'></img>
+        <img src={eagles_logo} className='team'></img>
       </div>
 
       <div className="bracket ravens">
-        <img src={bills_logo} className='team'></img>
+        <img src={ravens_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionalone">
-        <img src={steelers_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionaltwo">
-        <img src={bills_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionalthree">
-        <img src={steelers_logo} className='team'></img>
       </div>
 
       <div className="bracket niners">
-        <img src={bills_logo} className='team'></img>
+        <img src={niners_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionalfour">
-        <img src={steelers_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionalfive">
-        <img src={bills_logo} className='team'></img>
       </div>
 
       <div className="bracket divisionalsix">
-        <img src={steelers_logo} className='team'></img>
       </div>
+
+      <div className="bracket conferenceone">
+      </div>
+
+      <div className="bracket conferencetwo">
+      </div>
+
+      <div className="bracket conferencethree">
+      </div>
+
+      <div className="bracket conferencefour">
+      </div>
+
+      <div className="bracket superbowlone">
+      </div>
+
+      <div className="bracket superbowltwo">
+      </div>
+
+      <img src={superbowl_logo} className='superbowllogo'></img>
     </div>
   );
 }
